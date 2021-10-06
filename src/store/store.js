5db26c20280e2b6bc08de98d368844f6b7b723c3
Beefaz <justinas.geryba@gmail.com>
2021-10-06 15:49:58 +0300

    added: localStorage saving

--- src/store/store.js
+++ src/store/store.js
@@ -9,9 +9,9 @@ export default new Vuex.Store({
     allIssues: [
       {
         id: 0,
-        text: 'turiu paruosti namu darbus',
+        text: 'turiu paruosti namu darbus dfgdfhf sh dsh sdfh dgh dsh sdfh dfh sdfh sdfh sdfh dsfh dsh dfh dfh sdfhdsf hdfh sh dfhsdf dfh sdfh sdfsh sdh',
         status: 'trashed',  //open,done,trashed
-        previousStatus: null
+        previousStatus: 'open'
       },
       {
         id: 1,
@@ -43,10 +43,6 @@ export default new Vuex.Store({
       let item = state.allIssues.findIndex(item => item.id === issue.id);
       state.allIssues[item] = {...issue, status: 'trashed', previousStatus: issue.status};
     },
-    ISSUE_EDIT: (state, issue) => {
-      let item = state.allIssues.findIndex(item => item.id === issue.id);
-      state.allIssues[item] = {...issue};
-    },
     ISSUE_MARK_AS_OPEN: (state, issue) => {
       let item = state.allIssues.findIndex(item => item.id === issue.id);
       state.allIssues[item] = {...issue, status: 'open'};
@@ -60,8 +56,22 @@ export default new Vuex.Store({
       state.allIssues[item] = {...issue, status: state.allIssues[item].previousStatus};
     },
     ISSUE_SET_CURRENT: (state, issue) => {
-      state.currentIssue = issue;
+      state.currentIssue = {...issue};
     },
+    ISSUE_SAVE_CURRENT: (state, issue) => {
+      let item = state.allIssues.findIndex(item => item.id === issue.id);
+      state.allIssues[item] = {...state.currentIssue};
+      state.currentIssue = {};
+    },
+
+    // eslint-disable-next-line no-unused-vars
+    GET_COOKIE_DATA:(state) => {
+      if (localStorage.getItem('issues-storage')) state.allIssues = JSON.parse(localStorage.getItem('issues-storage')).allIssues;
+    },
+    SET_COOKIE_DATA:(state) => {
+      if (localStorage.getItem('issues-storage')) localStorage.removeItem('issues-storage');
+      localStorage.setItem('issues-storage', JSON.stringify(state));
+    }
   },
   actions: {},
   modules: {},
--- src/views/dashboard/Dashboard.vue
+++ src/views/dashboard/Dashboard.vue
@@ -24,76 +24,68 @@
 
     <!-- other cards-->
     <b-row class="mt-1"
-        v-for="(issue, index) in allIssues.filter(item => item.status === $route.meta.statusView)"
-        :key="index"
+           v-for="(issue, index) in allIssues.filter(item => item.status === $route.meta.statusView)"
+           :key="index"
     >
-        <b-card v-if="issue.status === $route.meta.statusView" class="custom-issue-card">
-          <b-card-text>
+      <b-card v-if="issue.status === $route.meta.statusView" class="custom-issue-card">
+        <b-card-text>
 
-            <input
-                ref="input"
-                v-if="currentIssue.id === issue.id"
-                :value="`${issue.text}`"
-                :v-model="editableIssue.text"
-            />
-            <span
-                v-else
-                @click="setCurrentIssue(issue)"
-            >
+          <input
+              v-if="currentIssue.id === issue.id"
+              :value="`${issue.text}`"
+              :v-model="editableIssue.text"
+              v-on:keyup.enter="saveCurrentIssue()"
+          />
+          <span
+              v-else
+              @click="setCurrentIssue(issue)"
+          >
           {{ issue.text }}
         </span>
-          </b-card-text>
+        </b-card-text>
 
-          <b-card-footer>
-            <!-- save btn-->
-            <b-button
-                v-if="editableIssue.text"
-                variant="success"
-                @click="editIssue(issue)"
-            >
-              Save
-            </b-button>
+        <b-card-footer>
 
-            <!-- mark as done btn-->
-            <b-button
-                v-else-if="issue.status === 'open'"
-                variant="primary"
-                @click="markAsDoneIssue(issue)"
-                v-b-tooltip.hover title="Mark as done"
-            >
-              <b-icon icon="check2"></b-icon>
-            </b-button>
+          <!-- mark as done btn-->
+          <b-button
+              v-if="issue.status === 'open'"
+              variant="primary"
+              @click="markAsDoneIssue(issue)"
+              v-b-tooltip.hover title="Mark as done"
+          >
+            <b-icon icon="check2"></b-icon>
+          </b-button>
 
-            <!-- mark as open btn-->
-            <b-button
-                v-if="issue.status === 'done'"
-                variant="warning"
-                @click="markAsOpenIssue(issue)"
-                v-b-tooltip.hover title="Mark as open"
-            >
-              <b-icon icon="check2"></b-icon>
-            </b-button>
+          <!-- mark as open btn-->
+          <b-button
+              v-if="issue.status === 'done'"
+              variant="warning"
+              @click="markAsOpenIssue(issue)"
+              v-b-tooltip.hover title="Mark as open"
+          >
+            <b-icon icon="check2"></b-icon>
+          </b-button>
 
-            <!-- delete btn-->
-            <b-button
-                v-if="issue.status === 'open' || issue.status === 'done'"
-                variant="danger"
-                @click="deleteIssue(issue)"
-                v-b-tooltip.hover title="Move to trash"
-            >
-              <b-icon icon="trash"></b-icon>
-            </b-button>
+          <!-- delete btn-->
+          <b-button
+              v-if="issue.status === 'open' || issue.status === 'done'"
+              variant="danger"
+              @click="deleteIssue(issue)"
+              v-b-tooltip.hover title="Move to trash"
+          >
+            <b-icon icon="trash"></b-icon>
+          </b-button>
 
-            <!-- restore btn-->
-            <b-button
-                v-if="issue.status === 'trashed'"
-                @click="restoreIssue(issue)"
-                v-b-tooltip.hover title="Restore"
-            >
-              <b-icon icon="arrow90deg-left"></b-icon>
-            </b-button>
-          </b-card-footer>
-        </b-card>
+          <!-- restore btn-->
+          <b-button
+              v-if="issue.status === 'trashed'"
+              @click="restoreIssue(issue)"
+              v-b-tooltip.hover title="Restore"
+          >
+            <b-icon icon="arrow90deg-left"></b-icon>
+          </b-button>
+        </b-card-footer>
+      </b-card>
     </b-row>
   </b-container>
 </template>
@@ -130,11 +122,12 @@ export default {
     ...mapMutations([
       'ISSUE_NEW',
       'ISSUE_DELETE',
-      'ISSUE_EDIT',
       'ISSUE_MARK_AS_OPEN',
       'ISSUE_MARK_AS_DONE',
       'ISSUE_RESTORE',
-      'ISSUE_SET_CURRENT'
+      'ISSUE_SET_CURRENT',
+      'ISSUE_SAVE_CURRENT',
+      'GET_COOKIE_DATA'
     ]),
     addNewIssue() {
       this.ISSUE_NEW({...this.newIssue});
@@ -146,14 +139,14 @@ export default {
     markAsDoneIssue(issue) {
       this.ISSUE_MARK_AS_DONE(issue);
       this.reload++;
-      console.log(this.$store.state);
     },
     deleteIssue(issue) {
       this.ISSUE_DELETE(issue);
       this.reload++;
     },
-    editIssue() {
-      this.ISSUE_EDIT(this.editableIssue);
+    saveCurrentIssue() {
+      this.ISSUE_SAVE_CURRENT(this.editableIssue);
+      this.reload++;
     },
     restoreIssue(issue) {
       this.ISSUE_RESTORE(issue);
@@ -161,8 +154,15 @@ export default {
     },
     setCurrentIssue(issue) {
       this.ISSUE_SET_CURRENT(issue);
+      this.editableIssue = {...this.currentIssue};
     },
   },
+  beforeCreate() {
+    this.$store.commit('GET_COOKIE_DATA');
+  },
+  updated() {
+    this.$store.commit('SET_COOKIE_DATA');
+  }
 }
 </script>
 
@@ -181,11 +181,12 @@ export default {
       background-color: rgba(0, 0, 0, 0.03);
       border-top: 1px solid rgba(0, 0, 0, 0.125);
       margin-right: 5px;
+
       input {
         width: 100%;
         height: 100%;
-        background:transparent;
-        border:unset;
+        background: transparent;
+        border: unset;
       }
     }
   }
